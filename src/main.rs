6c6ac31794df492a7a88c93a77aed01a01a8f194
adisteyf <adisteyf@gmail.com>
2025-06-04 17:21:48 +0300

    added help & version args

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
-use std::{str,fs,process::Command};
-mod global;
-mod install_core;
+#![allow(unused_imports)]
+use std::{fs, process::Command, str};
 mod build_core;
+mod global;
+mod help;
+mod install_core;
+use crate::build_core::build_core;
 use crate::global::Config;
 use crate::global::read_config;
+use crate::help::{print_help, print_version};
 use crate::install_core::install_core;
-use crate::build_core::build_core;
 
 fn main() {
     let cmd: String;
     match std::env::args().nth(1) {
         Some(v) => cmd = v,
-        None    => { println!("ERROR: unexpected argument for command field."); return; },
+        None => {
+            println!("ERROR: unexpected argument for command field.");
+            return;
+        }
     }
 
     match cmd.as_str() {
         "install-core" => install_core(),
-        "build-core"   => build_core(),
-        _              => println!("ERROR: unexpected argument for command field."),
+        "build-core" => build_core(),
+        "--help" => print_help(),
+        "-h" => print_help(),
+        "--version" => print_version(),
+        "-v" => print_version(),
+        _ => println!("ERROR: unexpected argument for command field."),
     }
 }
-
