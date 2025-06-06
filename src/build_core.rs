c368c05a0990e628ad3a07a22b1516bd3c24fac8
adisteyf <adisteyf@gmail.com>
2025-06-06 17:05:04 +0300

    added rm_scripts func

--- src/build_core.rs
+++ src/build_core.rs
@@ -45,25 +45,15 @@ pub fn build_core() {
     let config: Config = read_config();
 
     println!("removing old fe-core scripts...");
-    let _rm_old_scripts = Command::new("bash")
-        .args(["-c", "rm -rf fe-core/cfe-*"])
-        .output()
-        .expect("cmd err");
-
-    let quoted_list: Vec<String> = config
-        .scripts
-        .list
-        .iter()
-        .map(|s| format!("'{}'", s))
-        .collect();
-    let folders_list = quoted_list.join(" ");
-
-    println!("copying new fe-core scripts...");
-    let _cp_new_scripts = Command::new("bash")
-        .args(["-c", &format!("cp -r {} fe-core", folders_list.as_str())])
-        .output()
-        .expect("cmd err");
-
+    /*
+        let _rm_old_scripts = Command::new("bash")
+            .args(["-c", "rm -rf fe-core/cfe-*"])
+            .output()
+            .expect("cmd err");
+    */
+    rm_scripts();
+    println!("pasting new scripts...");
+    cp_scripts(config);
     build_code();
     println!("done!");
 }
